c17c2d66259f333ee2842dda7c83f0414674f84b
Venice Hartwell <venicext@gmail.com>
2022-08-15 00:30:17 +0200

    v0.3

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 
 ## Hypothesis & Analysis
 ##### How does sentiment score relate to review score (out of 5)?
-Sentiment is closely related to reviewer score. The finding show ...
+The final comparison score of ~3.3 shows users review their products at a much higher rate than they express emotionally in their reviews, or the NLP analyzer does not pick up on sentiment well enough. While users review products at a 4 or 5 star rating (out of 5), their review sentiment score rarely reaches above 50% polarity. From the NLP's analysis we can assume that reviewers lack engaging praise or critique for their products, but if we dive deeper we may instead assume that reviews are quick and to the point, without much decorative text that may influence an NLP's polarity score. Instead, reviewers opt for helpful and utilitarian words or phrases that don;t often register on an NLP's polarity scanner. In the end, if Amazon wanted to compare users' review scores to the emotional response users have to their products, Amazon should request users to leave longer reviews. This would provide users the space to leave more flowery speech for the NLP to pick up.
 
 ## Resources
-• Libraries: flask, 
+• Libraries: flask, SQLalchemy, nltk, spacy, textblob, pandas, pymysql, config
--- main.py
+++ main.py
@@ -3,32 +3,108 @@ from flask import Flask
 import random
 from flask import jsonify
 
-#import tools.mongotools as mongo
-# mongo.all_sentences()
+# libraries for NLP
+#from nltk.corpus import stopwords
+import spacy
+from textblob import TextBlob
+from statistics import mean
 
 # libraries to access SQL
 import pymysql
 import pandas as pd
 from config.sqlconfig import engine
 
+
+# init flask
 app = Flask(__name__)
 
+
+# flask test
 @app.route("/")
 def greeting ():
     return f"Test successful"
 
+
+# display original database
 @app.route("/db")
 def database():
     
     df = pd.read_sql_query(
     """
-    SELECT reviewerName, helpful, reviewText, overall, summary, reviewTime FROM reviews ORDER BY helpful DESC;
+    SELECT reviewerName, helpful, reviewText, overall, summary, reviewTime FROM reviews ORDER BY helpful DESC LIMIT 100;
     """, engine)
     
     return str(df)
 
+
+# display all reviews from the database matching the given name
+@app.route("/db/{reviewerName}") # FIX: HOW TO WRITE PARAM?
+def database():
+    
+    df = pd.read_sql_query( # FIX: HOW TO WRITE PARAM?
+    """
+    SELECT reviewerName, helpful, reviewText, overall, summary FROM reviews WHERE reviewerName == reviewerName ORDER BY helpful DESC LIMIT 100;
+    """, engine)
+    
+    return str(df)
+
+
+# display database with added sentiment score, using NLP on review text
 @app.route("/db/sentiment")
 def sentiment():
+
+    # SQL -> DF
+    df = pd.read_sql_query(
+    """
+    SELECT reviewerName, helpful, reviewText, overall, summary FROM reviews ORDER BY helpful DESC LIMIT 100;
+    """, engine)
+
+    # init vars to use in function
+    word_list = []
+    sentiment_ready = []
+    lemmatized = []
+    nlp = spacy.load("en_core_web_sm")
+    stop = nlp.Defaults.stop_words # all stop words, for removing stop words from reviews
+
+    # find the sentiment of each review
+    for review in df['reviewText']: # for every review
+        review_to_split = review
+        for word in review_to_split.split(" "): # for every word in selected review
+            if word not in stop:
+                word_list.append(word)  # add split words from each review into a list of words (word_list)
+
+        string_without_stop = " ".join(word_list) # turn back into a string
+        tokens = nlp(string_without_stop) # tokenize each review
+
+        for token in tokens:
+            lemmatized.append(token.lemma_) # lemmatize each review
+        sentiment_ready.append(TextBlob(" ".join(lemmatized)).sentiment.polarity) #store the sentiment of each review in a list
+
+    # compare to overall (overall-sentiment*5) & add to df
+    sentiment_comparison = []
+    for index, rows in df.iterrows():
+        # print(rows['overall'], sentiment_ready[index]) # a test print, to visualize the difference before outputing a sum
+        sentiment_comparison.append(rows['overall']-sentiment_ready[index]*5) # original review score (out of 5) - new score from NLP analysis of review
+
+    # store comparison and new value back into df for later visualization
+    dict_to_merge = {'sentiment polarity':sentiment_ready, 
+                    'sentiment comparison':sentiment_comparison} # Add all lists to a new (temp) dataframe.
+
+    # concatenate dataframes
+    df2 = pd.DataFrame(dict_to_merge)
+    df_new = pd.concat([df, df2], axis=1)
+
+    # export df to new csv
+    df_new.to_csv('output/amazon_review_sentiment.csv', index="False")
+
+    print('average difference between user\'s review score (out of 5) vs review sentiment score: ',
+        mean(sentiment_comparison))
+    return str(df_new.head())
+
+
+# display database with new review added
+@app.route("/db/sentiment")
+def sentiment(): # FIX: ADD PARAMS OR DATA HERE?
     
     # SQL -> DF
     df = pd.read_sql_query(
@@ -36,21 +112,15 @@ def sentiment():
     SELECT reviewerName, helpful, reviewText, overall, summary, reviewTime FROM reviews ORDER BY helpful DESC;
     """, engine)
 
-    # NLP: sentiment of reviewText
-    sentiment_score = []
-    for index in df['reviewText']:
-        sentiment_score.append(???????)
-    
-    # compare to overall (overall-sentiment*5) & add to df
-    sentiment_comparison = []
-    for index, rows in df.itterows():
-        sentiment_comparison.append(rows['overall']-sentiment_score[index]*5)
-    
-    for elem in df:
-        sentiment_comparison.append(elem['overall']-sentiment_score[elem]*5)
-    
-    return avg(sentiment_comparison)
+    # add new review to df
+    dict_to_merge = {"reviewerName":reviewerName, "helpful":helpful, 
+        "reviewText":reviewText, "overall":overall, "summary":summary} # add review to new dict
+    df2 = pd.DataFrame(dict_to_merge) # translate dict to df
+    df_new = pd.concat([df, df2], axis=1) # concatenate dataframes
+    return str(df.tail()) #display df with new review
 
+
+# run the flask app in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
 
